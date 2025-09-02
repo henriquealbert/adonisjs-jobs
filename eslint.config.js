ad2a7eb3797c79bc7e742ec0e2bc3202b0eee8c0
Henrique Albert Schmaiske <ishenriquealbert@gmail.com>
2025-09-02 20:17:15 -0300

    refactor: update ESLint configuration to include specific rules and enhance TypeScript settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,11 @@
 import { configPkg } from '@adonisjs/eslint-config'
 
-export default configPkg()
+export default [
+  ...configPkg(),
+  {
+    files: ['src/**/*', 'tests/**/*'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+]
